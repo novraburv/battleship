0400b0227313b33cc8164c882b42648980194e9c
novraburv <78609440+novraburv@users.noreply.github.com>
2023-01-02 01:05:13 +0700

    add essential tools and files

---
+++ src/test/ship.test.js
@@ -0,0 +1,14 @@
+// const ship = require('../ship.js')
+
+describe('test hit() count', () => {
+  // assume ship length is 3
+  test.todo('first hit')
+  test.todo('second hit')
+  test.todo('last hit')
+})
+
+describe('test ship isSunk()', () => {
+  // assume ship length is 3
+  test.todo('ship hit once')
+  test.todo('ship receive 2nd and 3rd hit')
+})
