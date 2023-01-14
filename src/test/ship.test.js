cbd05752d1eb8de5ee502a6834ff98d1e6c78f4a
novraburv <78609440+novraburv@users.noreply.github.com>
2023-01-14 12:52:09 +0700

    complete ship test file

--- src/test/ship.test.js
+++ src/test/ship.test.js
@@ -2,13 +2,17 @@
 
 describe('test hit() count', () => {
   // assume ship length is 3
-  test.todo('first hit')
-  test.todo('second hit')
-  test.todo('last hit')
-})
-
-describe('test ship isSunk()', () => {
-  // assume ship length is 3
-  test.todo('ship hit once')
-  test.todo('ship receive 2nd and 3rd hit')
+  const ship0 = {}
+  beforeEach(() => {
+    ship0.hit()
+  })
+  test.todo('first hit', () => {
+    expect(ship0.getHP()).toBe(2)
+  })
+  test.todo('second hit', () => {
+    expect(ship0.getHP()).toBe(1)
+  })
+  test.todo('last hit', () => {
+    expect(ship0.getHP()).toBe(0)
+  })
 })
